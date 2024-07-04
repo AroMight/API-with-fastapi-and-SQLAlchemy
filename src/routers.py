b5e20dc54238631b7409df324f05930bbd8b4b7e
Denilson Silva <162153089+AroMight@users.noreply.github.com>
2024-07-04 20:47:19 -0300

    edit tag

--- src/routers.py
+++ src/routers.py
@@ -2,4 +2,4 @@ from fastapi import APIRouter
 from teams.controller import router as teams_router
 
 api_router = APIRouter()
-api_router.include_router(teams_router, prefix="/teams")
+api_router.include_router(teams_router, tags=["teams"])
