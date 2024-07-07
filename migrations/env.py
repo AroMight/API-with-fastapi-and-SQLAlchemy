e1679f081c2a9491bdb8ea279076c454bd341d0d
Denilson Silva <162153089+AroMight@users.noreply.github.com>
2024-07-07 11:47:46 -0300

    chore: Update .gitignore to ignore YAML files, Dockerfile, and .venv

--- migrations/env.py
+++ migrations/env.py
@@ -7,9 +7,9 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
-from configs.base_settings import settings
-from database.base_model import BaseModel
-from database import *
+from src.configs.base_settings import settings
+from src.database.base_model import BaseModel
+from src.database import *
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
--- src/players/schema.py
+++ src/players/schema.py
@@ -4,7 +4,7 @@ from pydantic import UUID4, Field
 from teams.schema import TeamIn
 
 
-class PlayerOut(BaseSchema):
+class Player(BaseSchema):
     first_name: Annotated[str | None, Field(
         max_length=50, example="John", description="Player's first name. Can be null.")] = None
     last_name: Annotated[str | None, Field(
@@ -13,13 +13,15 @@ class PlayerOut(BaseSchema):
         max_length=10, example="johndoe", description="Player's username")]
     email: Annotated[str, Field(
         max_length=50, example="jhon@doe.com.br", description="Player's email")]
-    uuid: Annotated[UUID4, Field(description="Player's ID")]
 
-
-class PlayerIn(PlayerOut):
+class PlayerIn(Player):
     password: Annotated[str, Field(
         min_length=8, max_length=16, description="Player's password")]
     team: Annotated[TeamIn | None, Field(description="Player's team")] = None
+
+class PlayerOut(Player):
+    uuid: Annotated[UUID4, Field(description="Player's ID")]
+
 
 
 class PlayerUpdate(BaseSchema):
